8a9217dfdf204aa9e754125b608e506fffbd02cf
Omanshu480 <omanshumahawar.181co237@nitk.edu.in>
2020-05-21 23:25:04 +0530

    Fixing Bug

--- pagerep/urls.py
+++ pagerep/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
-from django.conf.urls import include,url
 from . import views
-app_name='pagerep'
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('page2/',views.index2,name='index2'),
-    path('page2/algo/', views.algo2, name='algo2'),
-    path('algo/',views.algo),
-
 ]
