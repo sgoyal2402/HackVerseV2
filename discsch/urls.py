4f899157c8b57547384f74e9ae029a774ba92f20
Omanshu480 <omanshumahawar.181co237@nitk.edu.in>
2020-05-23 21:40:41 +0530

    Fixing Bug

--- discsch/urls.py
+++ discsch/urls.py
@@ -1,11 +1,6 @@
-from django.conf.urls import url,include
-from django.contrib import admin
+from django.urls import path
 from . import views
 
-app_name = 'disksch'
-urlpatterns =[
-    url(r'^$', views.home, name='index'),
-    url(r'^(?P<pk>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^demo/$', views.demo, name='demo'),
-    url(r'^gateway/$', views.gateway, name='gateway'),
+urlpatterns = [
+    path('', views.index, name='index'),
 ]
