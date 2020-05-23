d6d0ab7ae53ca10ce3eec16bc8be09e1fcdffba6
Omanshu480 <omanshumahawar.181co237@nitk.edu.in>
2020-05-23 10:28:38 +0530

    UI Changes

--- pagerep/urls.py
+++ pagerep/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('algo/', views.algo),
+    path('page2/',views.demo2,name='demo2'),
+    path('page2/algo/', views.algo2, name='algo2'),
 ]
--- pagerep/views.py
+++ pagerep/views.py
@@ -1,6 +1,58 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.http import JsonResponse
+from . utils import fifo as ff
+from . utils import lru,opt,lfu,sc
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Page Replacement home page")
+    return render(request = request,
+                  template_name = 'pagerep/index.html' )
+
+
+
+
+@csrf_exempt
+def algo(request):
+
+    if request.method == 'POST':
+        data = request.POST.get('requests')
+        requests = json.loads(data)
+        data = request.POST.get('size')
+        size = json.loads(data)
+
+        data = {'requests': requests, 'size':size}
+
+        result_ff = ff(data)
+        result_lru = lru(data)
+        result_opt = opt(data)
+        result_lfu=lfu(data)
+        result_sc=sc(data)
+        result = {'fifo': result_ff, 'lru':result_lru, 'opt':result_opt,'lfu':result_lfu,'sc':result_sc}
+    return JsonResponse(result)
+
+
+def demo2(request):
+    return render(request = request,
+                  template_name = 'pagerep/beladyanmly.html' )
+
+
+@csrf_exempt
+def algo2(request):
+
+    if request.method == 'POST':
+        data = request.POST.get('requests')
+        requests = json.loads(data)
+        data = request.POST.get('size')
+        size = json.loads(data)
+
+        data = {'requests': requests, 'size':size}
+
+        result_ff = ff(data)
+        result_lru = lru(data)
+
+        result = {'fifo': result_ff, 'lru':result_lru}
+    return JsonResponse(result)
