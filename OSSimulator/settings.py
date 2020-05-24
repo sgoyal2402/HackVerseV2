deeaeb75ebfbef5b62a52ab258ea255c07eaf41e
Omanshu480 <omanshumahawar.181co237@nitk.edu.in>
2020-05-24 12:14:43 +0530

    Adding WikiPages

--- OSSimulator/settings.py
+++ OSSimulator/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'deadlock',
     'pagerep',
     'cpusch',
+    'wikipages',
 ]
 
 MIDDLEWARE = [
--- wikipages/urls.py
+++ wikipages/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('banker', views.banker, name='banker'),
+    path('cpusch', views.cpusch, name='cpusch'),
+    path('deadlock', views.deadlock, name='deadlock'),
+    path('discsch', views.discsch, name='discsch'),
+    path('filealloc', views.filealloc, name='filealloc'),
+    path('pagerep', views.pagerep, name='pagerep'),
+    path('syscall', views.syscall, name='syscall'),
+    path('memalloc', views.banker, name='memalloc'),
 ]
