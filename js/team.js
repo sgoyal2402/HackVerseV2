770e9d29134dc557906f972a209b1c5e22a5008c
Ikjot Singh Dhody <ikjotsd@gmail.com>
2021-01-23 21:09:55 +0530

    basic team page layout done

---
+++ js/team.js
@@ -0,0 +1,427 @@
+var img_path = "../img/bg-img/4.jpg"
+var link = "https://google.com"
+var name = "FirstName LastName"
+
+let p_info = [{
+        name: 'Dr.K.Balaveera Reddy',
+        image_path: 'patrons/chairman.jpg',
+        email: '#',
+        linkedin: '#',
+        designation: 'Chairman BOG'
+    },
+    {
+        name: 'Prof. Karanam Umamaheshwar Rao',
+        image_path: 'patrons/Director.jpg',
+        email: 'director@nitk.ac.in',
+        linkedin: 'https://www.linkedin.com/in/k-umamaheshwar-rao-623a4373',
+        designation: 'Director'
+    }
+]
+
+let adv_info = [{
+        name: 'Prof. Ananthanarayana V.S.',
+        image_path: 'adv-com/anvs.jpg',
+        email: 'dydirector@nitk.edu.in',
+        linkedin: '#',
+        designation: 'Deputy Director'
+    },
+    {
+        name: 'Prof. A. Nityananda Shetty',
+        image_path: 'adv-com/ANS.jpg',
+        email: 'deanacademic@nitk.edu.in',
+        linkedin: '#',
+        designation: 'Dean Academic'
+    },
+    {
+        name: 'Prof. K. Panduranga Vittal',
+        image_path: 'adv-com/kpv1.jpg',
+        email: 'deanaair@nitk.edu.in',
+        linkedin: 'https://in.linkedin.com/pub/dr-panduranga-vittal-k/19/541/43a',
+        designation: 'Dean Alumni Affairs and Institutional Relations'
+    },
+    {
+        name: 'Prof. Aloysius Henry Sequeira',
+        image_path: 'adv-com/ahs.jpg',
+        email: 'ahs@nitk.ac.in',
+        linkedin: '#',
+        designation: 'Dean Faculty Welfare'
+    },
+    {
+        name: 'Prof. Subhash C. Yaragal',
+        image_path: 'adv-com/subhas.jpg',
+        email: 'dean_pd@nitk.edu.in',
+        linkedin: '#',
+        designation: 'Dean Planning & Development'
+    },
+    {
+        name: 'Prof. U. Shripathi Acharya',
+        image_path: 'adv-com/sripathi.png',
+        email: 'dean.rc@nitk.edu.in',
+        linkedin: '#',
+        designation: 'Dean Research & Consultancy'
+    },
+    {
+        name: 'Prof. Jagannath Nayak',
+        image_path: 'adv-com/jagannath.jpg',
+        email: 'jagan@nitk.ac.in',
+        linkedin: '#',
+        designation: 'Dean Students Welfare'
+    },
+    {
+        name: 'Prof. Ashvini Chaturvedi',
+        image_path: 'adv-com/ashv.png',
+        email: 'ashvini@nitk.ac.in',
+        linkedin: '#',
+        designation: 'Associate Dean'
+    },
+    {
+        name: 'Prof. Vidya Shetty K',
+        image_path: 'adv-com/vidyashetty.jpg',
+        email: 'vidyaks95@nitk.ac.in',
+        linkedin: '#',
+        designation: 'Associate Dean'
+    },
+    {
+        name: "Prof. S. M. Murigendrappa",
+        image_path: "adv-com/S M Murigendrappa-05.JPG",
+        email: "smm@nitk.ac.in",
+        linkedin: "#",
+        designation: "Chairman, Diamond jubilee Core Committee"
+    }
+]
+
+let fac_info = [{
+        name: 'Dr. Anand Kumar M.',
+        image_path: 'fac-ads/anandkumar.png',
+        email: 'm_anandkumar.nitk.edu.in',
+        linkedin: '#',
+        designation: 'Assistant Professor'
+    },
+    {
+        name: 'Dr. Sowmya Kamath S.',
+        image_path: 'fac-ads/sk-image.jpg',
+        email: 'sowmyakamath@nitk.edu.in',
+        linkedin: '#',
+        designation: 'Assistant Professor'
+    },
+    {
+        name: 'Dr. Suprabha K. R.',
+        image_path: 'fac-ads/suprabha-k-r-image.jpg',
+        email: 'suprabha@nitk.ac.in',
+        linkedin: '#',
+        designation: 'Assistant Professor'
+    }
+]
+
+let student_info = []
+student_info[0] = [{
+        "timestamp": "2019-12-31T10:01:25.107Z",
+        "email": "akash9721l@gmail.com",
+        "name": "Akash L",
+        "image_path": "students/500_542px - Akash L.jpg",
+        "linkedin": "https://www.linkedin.com/in/akash-l-285787126/",
+        "uploadImage": "https://drive.google.com/open?id=1fkyzQNC0w36yg1ZjfM4_y-bez6c1N6Vy"
+    },
+    {
+        "timestamp": "2019-12-31T11:47:28.648Z",
+        "email": "vachhanihpavan@gmail.com",
+        "name": "Pavan Vachhani",
+        "image_path": "students/DSC_00462 - Pavan Vachhani.jpg",
+        "linkedin": "https://www.linkedin.com/in/vachhanihpavan",
+        "uploadImage": "https://drive.google.com/open?id=1xoJVeylmGXiD3QWepJygdZfr83kygYd4"
+    },
+    {
+        "timestamp": "2019-12-31T14:42:01.307Z",
+        "email": "adarshmalapaka98@gmail.com",
+        "name": "Adarsh Malapaka",
+        "linkedin": "https://www.linkedin.com/in/adarsh-malapaka-653455152",
+        "image_path": "students/7ACF7F66-054D-42A2-9A79-2319FD278299 - IE NITK.jpeg",
+        "uploadImage": "https://drive.google.com/open?id=1v_OQwnyXICsgPL1Mm3bjT4RhXg-C3VIJ"
+    },
+    {
+        "timestamp": "2019-12-31T16:25:14.991Z",
+        "email": "saisushil10@gmail.com",
+        "name": "Sai Sushil",
+        "image_path": "students/Screenshot_20191021_153856 - Sai Sushil.jpg",
+        "linkedin": "https://www.linkedin.com/in/sai-sushil-10",
+        "uploadImage": "https://drive.google.com/open?id=1mzk0uB8P8Ml9QVBZPw-EyY8Bk9tk20QZ"
+    },
+    {
+        "timestamp": "2019-12-31T16:55:21.274Z",
+        "email": "shreyas.shankar920@gmail.com",
+        "name": "Shreyas Shankar",
+        "image_path": "students/shreyasshankar - Shreyas Shankar.jpg",
+        "linkedin": "https://www.linkedin.com/in/shreyas-shankar-474731140/",
+        "uploadImage": "https://drive.google.com/open?id=1Or0Jr_rbHYfa_yiohij2YOQUWujWq2TW"
+    },
+    {
+        "timestamp": "2019-12-31T18:08:47.055Z",
+        "email": "shravanshenoy1998@hotmail.com",
+        "name": "Shravan S",
+        "image_path": "students/shravan1 - Shravan Shenoy.png",
+        "linkedin": "https://in.linkedin.com/in/shravan-shenoy-84295713a",
+        "uploadImage": "https://drive.google.com/open?id=1qX04DIvuUztPHPKtK9r5ckIuwRyURv04"
+    },
+    {
+        "email": "shreyasseshadri@gmail.com",
+        "name": "Shreyas Seshadri",
+        "image_path": "students/shreyas-seshadri.jpg",
+        "linkedin": "https://www.linkedin.com/in/shreyas-seshadri-8b278a130/"
+    },
+    {
+        "timestamp": "2020-01-01T02:43:39.732Z",
+        "email": "namrataladda@gmail.com",
+        "name": "Namrata Ladda",
+        "image_path": "students/IMG_20191016_190625 - Namrata Ladda.jpg",
+        "linkedin": "https://www.linkedin.com/in/namrata-ladda-255593153",
+        "uploadImage": "https://drive.google.com/open?id=1IoC5x6fixzXBziL2Mo03bWTBW_nVNOkB"
+    },
+    {
+        "timestamp": "2020-01-01T02:43:39.732Z",
+        "email": "keerthana.keetu.p@gmail.com",
+        "name": "Keerthana Polkampally",
+        "image_path": "students/keerthana.jpg",
+        "linkedin": "https://www.linkedin.com/in/polkampally-keerthana-66973112b/",
+        "uploadImage": "https://drive.google.com/open?id=1IoC5x6fixzXBziL2Mo03bWTBW_nVNOkB"
+    },
+    {
+        "timestamp": "2020-01-01T02:43:39.732Z",
+        "email": "mehnaz138@gmail.com",
+        "name": "Mehnaz Yunus",
+        "image_path": "students/IMG_20191124_145951_4-Mehnaz-Yunus.jpg",
+        "linkedin": "https://www.linkedin.com/in/mehnaz-yunus/",
+        "uploadImage": "https://drive.google.com/open?id=1IoC5x6fixzXBziL2Mo03bWTBW_nVNOkB"
+    },
+    {
+        "timestamp": "1/4/2020 23:51:03",
+        "email": "shahmishal1998@gmail.com",
+        "name": "Mishal Shah",
+        "linkedin": "https://www.linkedin.com/in/mishal23",
+        "uploadImage": "https://drive.google.com/open?id=1BAMrc98s0hsMROiFoGTJRosv8iXcTVaW",
+        "image_path": "students/Mishal - HackVerse NITK.jpg"
+    },
+    {
+        "timestamp": "1/4/2020 23:51:03",
+        "email": "sohampatil798@gmail.com",
+        "name": "Soham Patil",
+        "linkedin": "https://www.linkedin.com/in/soham-shashikant-patil-a63b64133/",
+        "uploadImage": "https://drive.google.com/open?id=1BAMrc98s0hsMROiFoGTJRosv8iXcTVaW",
+        "image_path": "students/soham.jpeg"
+    },
+    {
+        'timestamp': '2020/01/23 12:02:46 AM GMT+5:30',
+        'email': 'varshamavath98@gmail.com',
+        'name': 'Varsha Mavath',
+        'linkedin': 'https://www.linkedin.com/in/varsha-mavath',
+        'image_path': 'students/Varsha Mavath.jpg',
+        'uploadImage': 'https://drive.google.com/u/2/open?usp=forms_web&id=1Mtp0NzHdnKAOugXfGVOtkzRPxFww2aVC'
+    },
+    {
+        "email": "bhatswathi1234@gmail.com",
+        "name": "Swathi Bhat",
+        "linkedin": "https://linkedin.com/in/swathibhat99/",
+        "image_path": "students/IMG-20200101-WA0038 - Swathi Bhat.jpg"
+    }
+]
+
+student_info[1] = [{
+        "email": "govindsunilkumar.171ec217@nitk.edu.in",
+        "name": "Govind Sunilkumar",
+        "linkedin": "https://www.linkedin.com/in/govind-sunilkumar-71431b186/",
+        "image_path": "students/WhatsApp Image 2020-01-05 at 22.19.10 - Govind Sunilkumar.jpeg"
+    },
+    {
+        "timestamp": "2019-12-31T14:04:05.182Z",
+        "email": "adityasg3@gmail.com",
+        "name": "Aditya Gourishetty",
+        "linkedin": "https://www.linkedin.com/in/aditya-gourishetty/",
+        "image_path": "students/Aditya_G - Aditya Gourishetty.png",
+        "uploadImage": "https://drive.google.com/open?id=1QMNvJCSx1P56TdAtR_fBPWty4wS8bExU"
+    },
+    {
+        "timestamp": "2019-12-31T14:18:30.756Z",
+        "email": "shreyansh25.shrivastava@gmail.com",
+        "name": "Shreyansh Shrivastava",
+        "linkedin": "https://www.linkedin.com/in/shreyansh-shrivastava/",
+        "image_path": "students/rsz_dp - HackVerse NITK.png",
+        "uploadImage": "https://drive.google.com/open?id=1bLCtkfm4WVSVBP9D6t0A7XkrGvpH8Zeq"
+    },
+    {
+        "timestamp": "2019-12-31T16:02:29.892Z",
+        "email": "sabareshreyash@gmail.com",
+        "name": "Shreyash Sabare",
+        "image_path": "students/Screenshot_20191228-204657 - Shreyash Sabare.jpg",
+        "linkedin": "https://www.linkedin.com/in/shreyash-sabare-91a960166",
+        "uploadImage": "https://drive.google.com/open?id=1oUfk4TwB3d4XQA-A5NRE1F8i01MTl5f9"
+    },
+    {
+        "timestamp": "2019-12-31T16:11:59.764Z",
+        "email": "anishpatil.171it109@nitk.edu.in",
+        "name": "Anish Patil",
+        "linkedin": "https://www.linkedin.com/in/patil-anish",
+        "image_path": "students/20191231_214049 - Anish Patil.jpg",
+        "uploadImage": "https://drive.google.com/open?id=1EnA_EozVNPYVvIHArLkDWUxo6ZiUcmC2"
+    },
+    {
+        "timestamp": "2019-12-31T16:15:41.855Z",
+        "email": "k_rahul_reddy@outlook.com",
+        "name": "K Rahul Reddy",
+        "linkedin": "https://www.linkedin.com/in/rahul-reddy-181522161",
+        "image_path": "students/K Rahul Reddy.jpeg",
+        "uploadImage": "https://drive.google.com/open?id=1wFilE7WgM8sipW6z37tx01Fz4BaY9CYf"
+    },
+    {
+        "timestamp": "2020-01-01T07:06:43.291Z",
+        "email": "shubham050300@gmail.com",
+        "name": "Shubham Agrawal",
+        "linkedin": "https://www.linkedin.com/in/shubham050300/",
+        "image_path": "students/PicsArt_01-01-12.35.49 - shubham agrawal.jpg",
+        "uploadImage": "https://drive.google.com/open?id=1t2WitRXRoXwSEV-Ntzcxfh3F-MNnC-Ng"
+    },
+    {
+        "timestamp": "2020-01-01T14:06:37.750Z",
+        "email": "vybhavpai1999.vp@gmail.com",
+        "name": "Vybhav Pai",
+        "linkedin": "https://www.linkedin.com/in/vybhav-pai-a20331178",
+        "image_path": "students/hackverse - Vybhav Pai.jpg",
+        "uploadImage": "https://drive.google.com/open?id=1ptbJurzJ-l8DSQf4zVBiYLof-w3tVuCz"
+    },
+    {
+        "timestamp": "2020-01-03T17:49:51.311Z",
+        "email": "madhumithanara.171it125@nitk.edu.in",
+        "name": "Madhumitha Nara",
+        "image_path": "students/IMG_20180529_142057_542x500 - Madhumitha Nara.jpg",
+        "linkedin": "https://www.linkedin.com/in/madhumitha-nara/",
+        "uploadImage": "https://drive.google.com/open?id=130fM2DWBeQ-0f_g-VSkZ9oVqM99ojUPX"
+    },
+    {
+        'timestamp': '2020/01/22 2:28:34 PM GMT+5:30',
+        'email': 'ratan.171ee234@nitk.edu.in',
+        'name': 'Ratan Mamdekar',
+        'linkedin': 'https://www.linkedin.com/in/ratan-mamdekar-218b6b49/',
+        'image_path': 'students/Ratan Mamdekar1.jpg',
+        'uploadImage': 'https://drive.google.com/u/2/open?usp=forms_web&id=1qIbrU1OWDDOqDzT8_Enq0r0GRut4A_WF'
+    },
+    {
+        'timestamp': '2020/01/22 3:06:26 PM GMT+5:30',
+        'email': 'nishantkumar.171ee230@nitk.edu.in',
+        'name': 'Nishant Kumar',
+        'linkedin': '#',
+        'image_path': 'students/Nishant Kumar.jpg',
+        'uploadImage': 'https://drive.google.com/u/2/open?usp=forms_web&id=1UhHy-rDGPvmCWrGf717TGPCQUVWKPpc_'
+    },
+    {
+        'email': 'chaitany.pandiya@gmail.com',
+        'name': 'Chaitany Pandiya',
+        'linkedin': 'https://www.linkedin.com/in/chaitany-pandiya-6b2788147/',
+        'image_path': 'students/Chaitany.jpg',
+    },
+
+]
+
+
+student_info[2] = [{
+        "timestamp": "2019-12-31T13:52:30.131Z",
+        "email": "feyazabubaker@gmail.com",
+        "name": "Feyaz Baker",
+        "linkedin": "https://www.linkedin.com/in/feyaz-baker-520086175/",
+        "image_path": "students/69637394_2507244952838478_8668472265190408192_n - Feyaz Baker.jpg",
+        "uploadImage": "https://drive.google.com/open?id=1VPyV8skRmvi7WJBttZOOHdIdcKUK3Y52"
+    },
+    {
+        "timestamp": "2020-01-01T17:10:19.988Z",
+        "email": "sudeepthinallareddy@gmail.com",
+        "name": "Nalla Sudeepthi",
+        "image_path": "students/IMG-20200101-WA0005 - Sudeepthi Nallareddy.jpg",
+        "linkedin": "https://www.linkedin.com/in/sudeepthi-nallareddy-4b3a83192",
+        "uploadImage": "https://drive.google.com/open?id=1DLWDee5UwUsPoClcJwV9jzQRMjpLgOrS"
+    },
+    {
+        "timestamp": "2019-12-31T18:14:34.198Z",
+        "email": "shumbul.181co152@nitk.edu.in",
+        "name": "Shumbul Arifa",
+        "image_path": "students/ShumbulArifa.jpg",
+        "linkedin": "https://www.linkedin.com/in/shumbul-arifa-420511143/",
+        "uploadImage": "https://drive.google.com/open?id=18iYvly_i2AS1H-mwVMRcuqtRN44DCLdi"
+    },
+    {
+        'timestamp': '2020/01/22 1:35:17 AM GMT+5:30',
+        'email': 'prakritigoyal19@gmail.com',
+        'name': 'Prakriti Goyal',
+        'linkedin': 'https://www.linkedin.com/in/prakriti-goyal-595340192/',
+        'image_path': 'students/Prakriti Goyal1.jpg',
+        'uploadImage': 'https://drive.google.com/u/2/open?usp=forms_web&id=1UX7cvzG7i8ZaEp8K7DNVc3ct1SUvEvRS'
+    },
+    {
+        'timestamp': '2020/01/22 2:52:32 AM GMT+5:30',
+        'email': 'omanshumahawar.181co237@nitk.edu.in',
+        'name': 'Omanshu Mahawar ',
+        'linkedin': 'http://linkedin.com/in/omanshu-mahawar-7b6325192',
+        'image_path': 'students/Omanshu Mahawar.jpeg',
+        'uploadImage': 'https://drive.google.com/u/2/open?usp=forms_web&id=1-9ClDctox_E1KS_DDqPjac1FgL908qA9'
+    },
+    {
+        'timestamp': '2020/01/22 1:06:53 PM GMT+5:30',
+        'email': 'jahnavikancharla99@gmail.com',
+        'name': 'Jahnavi Kancharla ',
+        'linkedin': 'http://linkedin.com/in/jahnavi-kancharla-b68379191',
+        'image_path': 'students/Kancharla Jahnavi.jpeg',
+        'uploadImage': 'https://drive.google.com/u/2/open?usp=forms_web&id=1tNhIUyiKYDvWQVsaHkgnzDXucbKnshUt'
+    }
+]
+
+function insert_in_div(insert_div, persons) {
+    persons.forEach((person) => {
+        if (person.designation === undefined) person.designation = ''
+        div_text = `
+            <div class="col-12 col-sm-6 col-lg-3">
+            <div class="single-speaker-area bg-gradient-overlay-2 wow fadeInUp" data-wow-delay="300ms">
+                <!-- Thumb -->
+                <div class="speaker-single-thumb">
+                    <img src="img/team_page/` + person.image_path + `" alt="" style="height:325px">
+                </div>
+                <!-- Social Info -->
+                <div class="social-info">
+                    <a href="` + "mailto:" + person.email + `"><i class="zmdi zmdi-email"></i></a>
+                    <a href="` + person.linkedin + `"><i class="zmdi zmdi-linkedin"></i></a>
+                </div>
+                <!-- Info -->
+                <div class="speaker-info">
+                    <h5>` + person.name + `</h5>
+                    <p style="color:gold">` + person.designation + `<p>
+                </div>
+            </div>
+            </div>
+            `
+        insert_div.innerHTML += div_text
+    })
+}
+
+function compare(a, b) {
+    if (a.name < b.name) {
+        return -1;
+    }
+    if (a.name > b.name) {
+        return 1;
+    }
+    return 0;
+}
+
+var p_div = document.getElementById('p').getElementsByClassName('row')[0]
+insert_in_div(p_div, p_info)
+
+var adv_div = document.getElementById('adv').getElementsByClassName('row')[0]
+insert_in_div(adv_div, adv_info)
+
+var fac_div = document.getElementById('facad').getElementsByClassName('row')[0]
+insert_in_div(fac_div, fac_info)
+
+var student_div = document.getElementById('students').getElementsByClassName('row')[0]
+
+student_info[0].sort(compare)
+student_info[1].sort(compare)
+student_info[2].sort(compare)
+
+insert_in_div(student_div, student_info[0])
+insert_in_div(student_div, student_info[1])
+insert_in_div(student_div, student_info[2])
